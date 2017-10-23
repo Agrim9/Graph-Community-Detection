9a77cd89ed9c0761dbe33b9b51823e599cef9b44
Agrim9 <agrim9gupta@gmail.com>
2017-10-23 19:56:26 +0530

    New Hope

--- community_search.py
+++ community_search.py
@@ -9,9 +9,9 @@ def community_search(X,k,w,th,n):
 	#Partition nodes into four sets P1, P2, P3, P4 at random.Let ni=|Pi|
 	indice_vec=np.array(range(n))
 	random.shuffle(indice_vec)
-	place_holder1=random.randint(1,n-3)
-	place_holder2=random.randint(place_holder1,n-2)
-	place_holder3=random.randint(place_holder2,n-1)
+	place_holder1=random.randint(10,n-30)
+	place_holder2=random.randint(place_holder1+10,n-20)
+	place_holder3=random.randint(place_holder2+10,n-10)
 
 	#place_holder1=n//4
 	#place_holder2=n//2
@@ -28,28 +28,42 @@ def community_search(X,k,w,th,n):
 	n4=n-place_holder3
 	print ("N1 is " + str(n1)+" N2 is " + str(n2)+" N3 is " + str(n3)+" N4 is " + str(n4))
 	
+	nodes_in_V1=np.zeros(n)
+	Vp1=PreProcess(P1,P2,P3,P4,n1,n2,n3,n4,X,w,k,th)
+	for i in range(P1.size):
+		if(Vp1[i]==1):
+			nodes_in_V1[P1[i]]=1
+	Vp2=PreProcess(P2,P3,P4,P1,n2,n3,n4,n1,X,w,k,th)	
+	for i in range(P2.size):
+		if(Vp2[i]==1):
+			nodes_in_V1[P2[i]]=1
+	Vp3=PreProcess(P3,P4,P1,P2,n3,n4,n1,n2,X,w,k,th)	
+	for i in range(P3.size):
+		if(Vp3[i]==1):
+			nodes_in_V1[P3[i]]=1
+	Vp4=PreProcess(P4,P1,P2,P3,n4,n1,n2,n3,X,w,k,th)	
+	for i in range(P4.size):
+		if(Vp4[i]==1):
+			nodes_in_V1[P4[i]]=1
+	print (np.count_nonzero(Vp1)+np.count_nonzero(Vp2)+np.count_nonzero(Vp3)+np.count_nonzero(Vp4))	
+	print (nodes_in_V1)
+	return nodes_in_V1
+
+def PreProcess(P1,P2,P3,P4,n1,n2,n3,n4,X,w,k,th):
 	#----------------------------------------------------------------
 	#Compute matrices A1=(X1,3)/√n3, A2 =(X2,3)/√n3
 	row_idx=P1
 	col_idx=P3
 	A1=X[row_idx[:,None],col_idx]/np.sqrt(n3)
-	print (A1.shape)
-	print(np.count_nonzero(A1))
 	row_idx=P2
 	A2=X[row_idx[:,None],col_idx]/np.sqrt(n3)
-	print (A2.shape)
-	print(np.count_nonzero(A2))
-	_,D,_=la.svd(A1)
-	print(D)
-	_,D,_=la.svd(A2)
-	print(D)
 	#----------------------------------------------------------------
 	#Compute vector m1=(\sum_{j \in P1} X1,j)/n1
 	row_idx=P1
 	col_idx=P1
 	temp=X[row_idx[:,None],col_idx]
 	m1=np.matrix([np.sum(temp[i]) for i in range(row_idx.size)]).T
-
+	
 	#----------------------------------------------------------------
 	#Compute matrix B=(\sum_{j \in P4} wjX1,j(X2,j).T)/n4
 	B=np.zeros(shape=(n1,n2))
@@ -64,36 +78,36 @@ def community_search(X,k,w,th,n):
 
 	#----------------------------------------------------------------
 	#Compute Vp1
-	Vp1=[1 if up1[i]>th else 0 for i in P1]
-	print (Vp1)
+	Vp=[1 if up1[i]>th else 0 for i in range(P1.size)]
+	#print (up1)
+	return Vp
 
-
+	
 def SearchSubroutine(A1,A2,B,m1,k):
 	#----------------------------------------------------------------
 	#Compute Rank k svds of A1,A2
 	U1,S1,V1t=svds(A1,k)
 	D1=np.diag(S1)
-	print(S1)
+	#print(S1)
 	V1=V1t.T
 	U2,S2,V2t=svds(A2,k)
 	D2=np.diag(S2)
-	print(S2)
+	#print(S2)
 	V2=V2t.T
 
 	#----------------------------------------------------------------
 	#Compute matrices W1=U1*inv(D1), W2=U2*inv(D2)
-	W1=U1*la.inv(D1)
-	W2=U2*la.inv(D2)
+	W1=np.matmul(U1,la.inv(D1))
+	W2=np.matmul(U2,la.inv(D2))
 
 	#----------------------------------------------------------------
 	#u1 <- largest left singular vector of W1.T*B*W2
 	mat=W1.T*B*W2
 	u1_v,_,_=svds(mat,1)
 	u1=np.matrix(u1_v)
-
 	#----------------------------------------------------------------
 	#compute z=U1*D1*u1, a=u1.T*W1.T*m1
-	z=U1*D1*u1
+	z=np.matmul(np.matmul(U1,D1),u1)
 	a=u1.T*W1.T*m1
 
 	return z/a,(a*a)
--- implementation.py
+++ implementation.py
@@ -4,10 +4,10 @@ import numpy as np
 import random
 from community_search import community_search 
 n=1000
-k=8
+k=5
 alpha_min=0.1
 q=0.01
-p=0.3
+p=0.5
 
 #get Pref Matrix, i*p 
 Pref_mat=np.matrix(p*np.identity(k)+q*(np.ones(shape=(k,k))-np.identity(k)))
@@ -35,24 +35,24 @@ print (block_vec)
 g=ig.Graph.SBM(n,Pref_mat.tolist(),block_vec)
 
 #Label m vertices as blue, other as pink
-m=10
+m=20
 r=3 #5 Hops
-g.vs["color"]=["pink" if i<m else "red" if i<min_commsize else "blue" for i in range(n)]
 '''Wt assignment
 #print (g.get_shortest_paths(g.vs[100],g.vs[0:10]))
 #print ([1 if len(x)<4 else 0 for x in g.get_shortest_paths(g.vs[100],g.vs[0:10])])
 #print (sum([1 if len(x)<4 else 0 for x in g.get_shortest_paths(g.vs[100],g.vs[0:10])]))
 less than r hops
 '''
-w=[sum([1 if len(x)<r else 0 for x in g.get_shortest_paths(g.vs[i],g.vs[0:10])]) for i in range(n)]
+w=[sum([1 if len(x)<r else 0 for x in g.get_shortest_paths(g.vs[i],g.vs[0:m])]) for i in range(n)]
 g.vs["side_info_wt"]=w
 X_list=g.get_adjacency()
 X_arr=np.array(X_list[0])
 for i in range(1,n):
-	X_arr=np.vstack([X_arr,X_list[1]])
+	X_arr=np.vstack([X_arr,X_list[i]])
 X=np.matrix(X_arr)
-#print (X.shape)
-community_search(X,k,w,0.1,n)
+#
+nodes_in_V1=community_search(X,k,w,0.001,n)
+g.vs["color"]=["red" if  nodes_in_V1[i]==1 else "blue" for i in range(nodes_in_V1.size)]
+ig.plot(g)
 #ig.summary(g
-#ig.plot(g)
 
